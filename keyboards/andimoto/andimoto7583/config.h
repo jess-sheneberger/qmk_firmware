08fba2d5e6598cec62888dadbdee751723a37c4d
Andreas Zeiler <andreas.zeiler@posteo.de>
2020-12-21 15:49:02 +0100

    andimoto7583.h, config.h und 2 weitere dateien aktualisiert...

--- keyboards/andimoto/andimoto7583/config.h
+++ keyboards/andimoto/andimoto7583/config.h
@@ -27,8 +27,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PRODUCT      andimoto7583
 
 /* key matrix size */
-#define MATRIX_ROWS 2
-#define MATRIX_COLS 3
+#define MATRIX_ROWS 6
+#define MATRIX_COLS 14
 
 /*
  * Keyboard Matrix Assignments
@@ -40,8 +40,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
  *
  */
-#define MATRIX_ROW_PINS { D0, D5 }
-#define MATRIX_COL_PINS { F1, F0, B0 }
+#define MATRIX_ROW_PINS { D3, D2, D1, D0, D4, C6 }
+#define MATRIX_COL_PINS { D7, E6, B4, B5, B0,D5, B6, B2, B3, B1, F7, F6, F5, F4 }
 #define UNUSED_PINS
 
 /* COL2ROW, ROW2COL */
